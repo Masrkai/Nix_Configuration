88f0c4f69c1723f9f97e25d32a6a50cd2a1ab421
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2024-09-03 00:32:41 +0300

    Removed an unneeded file

--- init_db.sql
+++
@@ -1,3 +0,0 @@
-create user ${secrets.databaseUser} with encrypted password '${secrets.databasePassword}';
-create database mydatabase;
-grant all privileges on database mydatabase to ${secrets.databaseUser};
