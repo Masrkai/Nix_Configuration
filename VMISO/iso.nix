528978a9dcaeb0f2c6bf05920a88fb454b73a5b6
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2024-09-26 15:20:17 +0300

    Base of main system iso

---
+++ VMISO/iso.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ../configuration.nix  # Import your existing configuration
+  ];
+
+  boot.loader.timeout = lib.mkForce 10;
+
+  # ISO-specific options
+  boot.loader.grub.memtest86.enable = true;
+  boot.supportedFilesystems = [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+
+  # Enable copy-on-write for the ISO
+  isoImage.squashfsCompression = "zstd -Xcompression-level 6";
+
+  # Optional: Set a custom ISO name
+  isoImage.isoName = "Main_NixOS_System.iso";
+}
