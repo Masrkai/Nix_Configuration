6da5b0022dfd1f2132891bb62d464b47b157081e
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-01-17 08:48:12 +0200

    configured systemd-oomd right

--- systemd.nix
+++ systemd.nix
@@ -4,16 +4,32 @@
 
  systemd = {
     enableEmergencyMode = true;
-    oomd = {
-      enable = true;                        # Enable systemd-oomd
-      enableRootSlice = true;               # Manage memory pressure for root processes
-      enableUserSlices = true;              # Manage memory for user sessions, reducing per-user memory pressure
-      enableSystemSlice = true;             # Monitor and manage system services to avoid OOM issues
-        extraConfig = {
-          MemoryPressureDurationSec="10s";             # Faster response to memory issues
-          DefaultMemoryPressureThresholdPercent=50;    # More aggressive memory protection
-        };
-    };
+      oomd = {
+        enable = true;                        # Enable systemd-oomd
+        enableRootSlice = true;               # Manage memory pressure for root processes
+        enableUserSlices = true;              # Manage memory for user sessions, reducing per-user memory pressure
+        enableSystemSlice = true;             # Monitor and manage system services to avoid OOM issues
+          extraConfig = {
+            MemoryPressureDurationSec="10s";             # Faster response to memory issues
+            DefaultMemoryPressureThresholdPercent=50;    # More aggressive memory protection
+          };
+      };
+      slices = {
+          "system.slice" = {
+            sliceConfig = {
+              MemoryPressureLimit = 95;        # Memory pressure limit to trigger actions
+              MemoryPressureDurationSec = "10s"; # How long pressure must persist before action
+            };
+          };
+
+          "user.slice" = {
+            sliceConfig = {
+              MemoryPressureLimit = 95;
+              MemoryPressureDurationSec = "10s";
+            };
+          };
+      };
+
 
     # Main systemd sleep configuration
     sleep.extraConfig = ''
