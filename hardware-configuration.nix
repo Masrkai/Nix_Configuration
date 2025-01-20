626f1522d5352968f10288a7b4c7c0c655fa5dff
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-01-20 18:45:34 +0200

    added bluetooth support

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -164,5 +164,8 @@
 
   services.power-profiles-daemon.enable= true;
 
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = false; # powers up the default Bluetooth controller on boot
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
