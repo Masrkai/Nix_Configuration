9cce11b409a1c69881998e27a00a7b0c56463032
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-01-22 20:01:53 +0200

    ssh settings

--- networking.nix
+++ networking.nix
@@ -495,6 +495,20 @@
     };
   };
 
+  #> SSH
+  services.openssh = {
+  enable = true;
+  ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
+
+
   #> DNS-over-TLS
   services.resolved.enable = lib.mkForce false;
   services.stubby = lib.mkForce {
