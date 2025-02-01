53526cdf418be5ebf36c6838cf16a5a2a51db23a
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-02-01 20:09:09 +0200

    complying with wireless regulations

---
+++ Wireless_Regulation.nix
@@ -0,0 +1,23 @@
+{ lib, config, pkgs, ... }:
+
+let
+  secrets = import ./Sec/secrets.nix;
+in
+{
+
+  boot.extraModprobeConfig = ''
+    options cfg80211 ieee80211_regdom=${secrets.WG}
+  '';
+
+  #  boot.extraModprobeConfig = ''
+  #   options cfg80211 ieee80211_regdom="${secrets.WG}"
+  # '';
+
+  hardware = lib.mkMerge [
+    {
+      wirelessRegulatoryDatabase = true;
+      firmware = with pkgs; [ wireless-regdb ];
+    }
+  ];
+
+}
