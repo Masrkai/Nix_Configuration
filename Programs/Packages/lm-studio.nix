d7688c9c172bac8baa66a551a1cb20e42626a687
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-02-01 20:11:54 +0200

    will be deleted soon --lm-studio

--- Programs/Packages/lm-studio.nix
+++ Programs/Packages/lm-studio.nix
@@ -14,6 +14,11 @@ let
     hash = "sha256-JnuEYU+vitBGS0WZdcleVW1DfZ+MonXz6U+ObUlsePM=";
   };
 
+  # icon = fetchurl {
+  #   url = "https://avatars.githubusercontent.com/u/133744619?s=280&v=4";
+  #   hash = "sha256-DNRimhNFt6jLdjqv7o2cNz38K6XnevxD0rGymym3xBs=";
+  # };
+
   appimageContents = appimageTools.extractType2 { inherit pname version src; };
 in
 appimageTools.wrapType2 {
@@ -31,20 +36,17 @@ appimageTools.wrapType2 {
     install -m 444 -D ${appimageContents}/lm-studio.desktop -t $out/share/applications
     substituteInPlace $out/share/applications/lm-studio.desktop \
       --replace-fail 'Exec=AppRun --no-sandbox %U' 'Exec=lmstudio'
+
   '';
+    # install -Dm444 ${icon} $out/share/icons/hicolor/scalable/apps/${pname}.svg
 
   meta = {
       description = "LM Studio is an easy to use desktop app for experimenting with local and open-source Large Language Models (LLMs)";
       homepage = "https://lmstudio.ai/";
       license = lib.licenses.unfree;
       mainProgram = "lmstudio";
-      maintainers = with lib.maintainers; [
-       offline
-      ];
-      platforms = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
+      maintainers = with lib.maintainers; [ offline ];
+      platforms = [ "x86_64-linux"];
       sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
     };
 }
