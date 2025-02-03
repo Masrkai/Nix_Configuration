e5d7f5a605340b1d926b23e52653a18e7a6cb86c
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-02-03 19:26:54 +0200

    added apache tika

--- Ai.nix
+++ Ai.nix
@@ -4,10 +4,20 @@
   services.ollama = {
     enable = true;
     acceleration = "cuda";
-    host = "127.0.0.1";
+
     port = 11434;
+    host = "127.0.0.1";
 
     user = "ollama";
     group = "ollama";
+  };
+
+  services.tika = {
+    enable = true;
+    enableOcr = true;
+
+    port = 9998;
+    openFirewall = false;
+    listenAddress = "127.0.0.1";
   };
 }
