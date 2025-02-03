98d4759cd62be0cc4d078bbdebdc8e957c0c68c6
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-02-03 19:26:23 +0200

    Evillimiter is back

---
+++ Programs/Packages/evillimiter.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  fetchFromGitHub,
+  iproute2,
+  nftables,
+  python3Packages,
+}:
+
+python3Packages.buildPythonApplication rec {
+  pname = "Evillimiter";
+  version = "1.6.1";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "Masrkai";
+    repo = "Evillimiter";
+    tag = "v${version}";
+    hash = "sha256-yWcsG6xlyzSrYxYZnnG8t2t1PubvYzqTcdO+JXzYsnk=";
+  };
+
+  build-system = with python3Packages; [ setuptools-scm ];
+
+  dependencies = with python3Packages; [
+    tqdm
+    scapy
+    netaddr
+    colorama
+    iproute2
+    nftables
+    netifaces
+    setuptools
+    terminaltables
+  ];
+
+  # Project has no tests
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Tool that monitors, analyzes and limits the bandwidth";
+    longDescription = ''
+      A tool to monitor, analyze and limit the bandwidth (upload/download) of
+      devices on your local network without physical or administrative access.
+      evillimiter employs ARP spoofing and traffic shaping to throttle the
+      bandwidth of hosts on the network.
+    '';
+    homepage = "https://github.com/Masrkai/Evillimiter";
+    license = licenses.mit;
+    maintainers = with maintainers; [ offline ];
+    mainProgram = "Evillimiter";
+  };
+}
