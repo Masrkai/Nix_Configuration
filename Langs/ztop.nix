496b40ed5ce3c892d5795c3c14269d7af6f878b1
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-02-27 21:40:32 +0200

    overlay to avoid compilation of torch

--- Langs/ztop.nix
+++ Langs/ztop.nix
@@ -1,4 +1,8 @@
+{  lib, pkgs, ...}:
 {
+
+
+
   imports = [
     ./env.nix
     ./vscodium.nix
@@ -7,8 +11,52 @@
 
   nixpkgs = {
     overlays = [
+      (final: prev: {
+        # Top-level package replacements
+        torch = prev.torch-bin;
+        triton = prev.triton-bin;
+        torchaudio = prev.torchaudio-bin;
+        torchvision = prev.torchvision-bin;
 
+        pythonPackagesExtensions = [
+
+          (py-final: py-prev: {
+            torch = py-final.torch-bin;
+            triton = py-final.triton-bin;
+            torchaudio = py-final.torchaudio-bin;
+            torchvision = py-final.torchvision-bin;
+          })
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+        # Compositionally correct extension for huggingface-hub
+        (py-final: py-prev: let
+          version = "0.29.1";
+        in {
+          huggingface-hub = py-prev.huggingface-hub.overrideAttrs (oldAttrs: {
+            inherit version;
+            src = prev.fetchFromGitHub {  # Use prev from outer scope
+              owner = "huggingface";
+              repo = "huggingface_hub";
+              tag = "v${version}";
+              # hash = lib.fakeHash;
+              hash = "sha256-9G5oq8X5/MtHZAOM7QHoMyRePPFwMe4Wa66y+japxwA=";
+            };
+            meta = oldAttrs.meta // {
+              changelog = "https://github.com/huggingface/huggingface_hub/releases/tag/v${version}";
+            };
+          });
+        })
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        ];
+
+
+      })
     ];
   };
+
+
+
+
 
 }
