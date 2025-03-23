991921cddb82683bbafd667d9800746c8ac2abd1
Masrkai <ahmedelsayedallamsquizeesfx@gmail.com>
2025-03-23 18:43:34 +0200

    added ext4

--- VMISO/iso.nix
+++ VMISO/iso.nix
@@ -10,7 +10,7 @@
 
   # ISO-specific options
   boot.loader.grub.memtest86.enable = true;
-  boot.supportedFilesystems = [ "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
+  boot.supportedFilesystems = [ "ext4" "btrfs" "reiserfs" "vfat" "f2fs" "xfs" "ntfs" "cifs" ];
 
   # Enable copy-on-write for the ISO
   isoImage.squashfsCompression = "zstd -Xcompression-level 6";
