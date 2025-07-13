c09d7a97cbbd4790843ea1cc183408f1af4c7266
Masrkai <118424417+Masrkai@users.noreply.github.com>
2025-07-13 16:20:11 +0300

    setting up rust properly

--- Langs/rust.nix
+++ Langs/rust.nix
@@ -2,15 +2,27 @@
 
 {
   rustpackages = with pkgs; [
+    # Compiler
     rustc
+
+    # Package manager
     cargo
-    clippy
-    rustfmt
+
+    # Development tools
+    clippy      # Linter for catching common mistakes
+    rustfmt     # Code formatter
+
+    # Language Server Protocol (LSP)
     rust-analyzer
+
+    # # Toolchain manager
+    # rustup      # Manages Rust versions and targets
   ];
 
   rust-nixpkgs-extensions = with pkgs.vscode-extensions; [
+    fill-labs.dependi
     rust-lang.rust-analyzer
+    tamasfe.even-better-toml
   ];
 
   rust-marketplace-extensions = with pkgs.vscode-utils.extensionsFromVscodeMarketplace; [
