b7b6acf5520f5b3276ea8583d5cc79d28ec969f9
Masrkai <118424417+Masrkai@users.noreply.github.com>
2025-08-03 19:33:11 +0300

    cpp env commented text cleanup

--- Dev/cpp_env.nix
+++ Dev/cpp_env.nix
@@ -19,23 +19,6 @@
     binutils         # Includes ld, ar, etc.
   ];
 
-  # This is the crucial part that was missing from your original setup
-  # We need to ensure the linker can find the standard libraries
-  # environment.variables = {
-  #   # Tell the linker where to find libraries
-  #   LIBRARY_PATH = "${pkgs.gcc.cc.lib}/lib:${pkgs.glibc}/lib";
-  #   # Tell the preprocessor where to find headers
-  #   C_INCLUDE_PATH = "${pkgs.glibc.dev}/include";
-  #   CPLUS_INCLUDE_PATH = "${pkgs.gcc.cc}/include/c++/${pkgs.gcc.cc.version}:${pkgs.glibc.dev}/include";
-  # };
-
-  # # For clang with libc++
-  # CPLUS_INCLUDE_PATH = lib.concatStringsSep ":" [
-  #   "${pkgs.libcxx.dev}/include/c++/v1"
-  #   "${pkgs.glibc.dev}/include"
-  # ];
-
-
  environment.variables = {
     # Tell CMake to use g++ instead of clang++
     CXX = "${pkgs.gcc}/bin/g++";
@@ -51,9 +34,8 @@
 
 
     LIBRARY_PATH = lib.makeLibraryPath [
-      pkgs.libcxx
-
       pkgs.glibc
+      pkgs.libcxx
       pkgs.gcc.cc.lib
       pkgs.stdenv.cc.cc.lib
     ];
