3457536ef49d4f87418f19016f09e9bad98cfc98
Masrkai <118424417+Masrkai@users.noreply.github.com>
2025-11-06 18:17:46 +0200

    updated the proton version

--- Programs/Packages/proton-ge-bin.nix
+++ Programs/Packages/proton-ge-bin.nix
@@ -9,12 +9,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "proton-ge-bin";
-  version = "GE-Proton10-17";
+  version = "GE-Proton10-24";
 
   src = fetchzip {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-    hash = "sha256-GMwAAKuaBhDv1TvAuW9DVcXSYPRM87NP6NnJfk8O8ZU=";
-    # hash = lib.fakeHash;
+    hash = "sha256-QZBu2C4JrsETY+EV0zs4e921qOxYT9lk0EYXXpOCKLs=";
   };
 
   dontUnpack = true;
