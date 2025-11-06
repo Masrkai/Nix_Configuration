18e91e904623a26cdd71b8766b19dc83ea582f1e
Masrkai <118424417+Masrkai@users.noreply.github.com>
2025-11-06 18:18:02 +0200

    wireless regulations back in check

--- Networking/Networking.nix
+++ Networking/Networking.nix
@@ -13,6 +13,7 @@
 
     ./Firewall.nix
     ./TimeSync.nix
+    ./Wireless_Regulation.nix
   ];
 
 
--- Networking/Wireless_Regulation.nix
+++ Networking/Wireless_Regulation.nix
@@ -1,25 +1,23 @@
-{ lib, config, pkgs, ... }:
+{ pkgs, ... }:
 
 let
   secrets = import ../Sec/secrets.nix;
 in
 {
 
-  # boot.extraModprobeConfig = ''
-  #   options rtw89_pci disable_clkreq=1 disable_aspm_l1=1 disable_aspm_l1ss=1
-  # '';
+  # Set regulatory domain
+  boot.extraModprobeConfig = ''
+    options cfg80211 ieee80211_regdom=US
+    options rtw89_pci disable_aspm_l1=y disable_aspm_l1ss=y disable_clkreq=y
+    options rtw89_core disable_ps_mode=y
+  '';
 
-
-
-  # boot.extraModprobeConfig = ''
-  #   options cfg80211 ieee80211_regdom=${secrets.WG}
-  # '';
-
-  # hardware = lib.mkMerge [
-  #   {
-  #     wirelessRegulatoryDatabase = true;
-  #     firmware = with pkgs; [ wireless-regdb ];
-  #   }
-  # ];
+  hardware = {
+      wirelessRegulatoryDatabase = true;
+      firmware = with pkgs; [
+      wireless-regdb
+      linux-firmware
+      ];
+    };
 
 }
